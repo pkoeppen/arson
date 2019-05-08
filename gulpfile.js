ea486752095fa4dd00cfe8e8766a12456ff2385f
Peter Koeppen <pkoeppen@lenovo.com>
2019-05-08 17:24:27 -0400

    modded and linted zvikov theme

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,15 @@ const gulp = require('gulp');
 const sync = require('browser-sync');
 const exec = require('child_process').exec;
 const sass = require('gulp-sass');
+const eslint = require('gulp-eslint');
 
+// Docker takes a second to get running. Wait for it to do so.
 const DOCKER_START_INTERVAL = 1000;
 const DOCKER_RESTART_INTERVAL = 2000;
 
 const docker = {
   up() {
+    // Spin up containers defined in docker-compose.yml.
     return new Promise(resolve => {
       exec('docker-compose up -d', error => {
         if (error) {
@@ -20,6 +23,7 @@ const docker = {
     });
   },
   restart() {
+    // Restart the Ghost container.
     return new Promise(resolve => {
       exec('docker restart ghost_blog', error => {
         if (error) {
@@ -32,14 +36,29 @@ const docker = {
 };
 
 gulp.task('reload', () => {
+    // Restart Docker, then refresh the page.
     return docker.restart()
     .then(() => sync.reload());
 });
 
+gulp.task('lint', () => {
+    // Lint main JS file.
+    return gulp.src(['./content/themes/arson/assets/js/main.js'])
+      .pipe(eslint())
+      .pipe(eslint.format());
+});
+
+gulp.task('js', () => {
+    // Stream JS to browser.
+    return gulp.src(['./content/themes/arson/assets/js/main.js'])
+      .pipe(sync.stream());
+});
+
 gulp.task('sass', () => {
-    return gulp.src("./content/themes/arson/assets/sass/*.sass")
+    // Compile Sass.
+    return gulp.src('./content/themes/arson/assets/sass/style.sass')
       .pipe(sass())
-      .pipe(gulp.dest("./content/themes/arson/assets/css"))
+      .pipe(gulp.dest('./content/themes/arson/assets/css'))
       .pipe(sync.stream());
 });
 
@@ -49,9 +68,10 @@ gulp.task('watch', () => {
         sync.init({
             proxy: 'localhost:2368'
         });
+        gulp.watch('./content/themes/arson/assets/js/*.js', gulp.parallel('lint', 'js'));
         gulp.watch('./content/themes/arson/assets/sass/*.sass', gulp.series('sass'));
         gulp.watch('./content/themes/arson/**/*.hbs', gulp.series('reload'));
     });
 });
 
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.series('sass', 'watch'));
